9c7303ca513a35642557fa07556b8bdc22708b52
kokyoungwoo <bizdb@kakao.com>
2020-11-13 11:48:23 +0900

    loading_API
    loading_API

---
+++ js/common.js
@@ -0,0 +1,32 @@
+$(function(){
+  $('.spinner.a').click(function(){
+    $('.big_loading').html('<div class="spinner a big"><p>loading</p><div class="one"></div><div class="two"></div></div>');
+  });
+  $('.spinner.b').click(function(){
+    $('.big_loading').html('<div class="spinner b big"><p>loading</p><div class="one"></div><div class="two"></div></div>');
+  });
+  $('.spinner.c').click(function(){
+    $('.big_loading').html('<div class="spinner c big"><p>loading</p><div class="one"></div><div class="two"></div></div>');
+  });
+  $('.spinner.d').click(function(){
+    $('.big_loading').html('<div class="spinner bar d big"><p>loading</p><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>');
+  });
+  $('.spinner.e').click(function(){
+    $('.big_loading').html('<div class="spinner bar e big"><p>loading</p><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>');
+  });
+  $('.spinner.f').click(function(){
+    $('.big_loading').html('<div class="spinner bar f big"><p>loading</p><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>');
+  });
+  $('.spinner.g').click(function(){
+    $('.big_loading').html('<div class="spinner g big"><span><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></span></div>');
+  });
+  $('.spinner.h').click(function(){
+    $('.big_loading').html('<div class="spinner h big"><div></div></div>');
+  });
+  $('.spinner.i').click(function(){
+    $('.big_loading').html('<div class="spinner i big"><div></div><div></div><div></div><div></div></div>');
+  });
+  $('.spinner.j').click(function(){
+    $('.big_loading').html('<div class="spinner j big"><p>loading</p><div class="one"></div><div class="two"></div></div>');
+  });
+});
